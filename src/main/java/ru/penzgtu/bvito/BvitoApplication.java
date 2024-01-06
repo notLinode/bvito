8b45ce41551ef94215d7ba70c6bd238097e14a7e
Iskenderov Andrei <linode@duck.com>
2024-01-06 10:50:36 +0300

    Init commit

---
+++ src/main/java/ru/penzgtu/bvito/BvitoApplication.java
@@ -0,0 +1,13 @@
+package ru.penzgtu.bvito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BvitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/penzgtu/bvito/BvitoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.penzgtu.bvito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BvitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
